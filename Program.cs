d469ed1d98baf59902a3168634c037164931613a
faizygraphics <fazitanveer2@gmail.com>
2021-11-25 00:37:34 +0500

    Q#52

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+        // (a)
+      int a = (1+8/2+((1*4)+(5*4))/4);
+      Console.WriteLine(a);
+      Console.ReadLine();
+
+        // (b)
+    int b = ((1+1+1+1)/2+(1+1+1)/3);
+    Console.WriteLine(b);
+    Console.ReadLine();
+
+       // (c)
+    int c = ( 5*5+5/5+6);
+    Console.WriteLine(c);
+    Console. ReadLine();
+
+       // (d)
+    int d = ((3+4)+(4+7))/5;
+    Console.WriteLine(d);
+    Console.ReadLine();
+
+       //(e)
+    int e = ( 3*6*7*2)+12/2;
+    Console. WriteLine(e);
+    Consol.ReadLine();
+
+      // (f)
+    int f = (8*4*2+6)/2+4;
+    Console.WriteLine(f);
+    Console.ReadLine();
+
+     }
+  }
+}
+
